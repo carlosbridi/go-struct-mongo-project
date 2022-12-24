ada5fbf1821b7af145ee6bfe831c336aec0b956c
Carlos Bridi <carlosbridi@gmail.com>
2022-12-24 16:00:02 -0300

    Mongo connection on main

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	mongoConnection "go_sample/src/main/config/mongo"
+	configurations_yaml "go_sample/src/main/config/yaml"
 	"go_sample/src/routes"
 	"log"
 	"net/http"
-
-	configurations_yaml "go_sample/src/main/config/yaml"
 )
 
 func main() {
 	r := routes.GenerateRoutes()
+
+	mongoConnection.GetDatabaseBean()
 
 	log.Printf("Escutando na porta %s", configurations_yaml.GetBeanPropertyByName("Application.Port"))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", configurations_yaml.GetBeanPropertyByName("Application.Port")), r))
